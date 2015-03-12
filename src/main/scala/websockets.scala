50164b640642c79d6152abb808dbda869fda79bc
no-glue <nikolapav1985@gmail.com>
2015-03-12 15:43:51 +0100

    removed semicolons

--- src/main/scala/websockets.scala
+++ src/main/scala/websockets.scala
@@ -1,18 +1,18 @@
-import com.pmeade.websocket.io.WebSocketServerOutputStream;
-import com.pmeade.websocket.net.WebSocket;
-import com.pmeade.websocket.net.WebSocketServerSocket;
-import com.pmeade.websocket.example.WebSocketConsumerThread;
-import com.pmeade.websocket.example.StringMessageQueue;
-import com.pmeade.websocket.example.ByteAccumulator;
-import com.pmeade.websocket.example.WebSocketThread;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Queue;
-import java.util.Arrays;
+import com.pmeade.websocket.io.WebSocketServerOutputStream
+import com.pmeade.websocket.net.WebSocket
+import com.pmeade.websocket.net.WebSocketServerSocket
+import com.pmeade.websocket.example.WebSocketConsumerThread
+import com.pmeade.websocket.example.StringMessageQueue
+import com.pmeade.websocket.example.ByteAccumulator
+import com.pmeade.websocket.example.WebSocketThread
+import java.io.IOException
+import java.io.InputStream
+import java.net.ServerSocket
+import java.util.Iterator
+import java.util.LinkedList
+import java.util.ListIterator
+import java.util.Queue
+import java.util.Arrays
 
 object Websockets {
   val PORT:Int = 8080
